3b6fa5b18a8d277b50c4a39936fcfda75041f784
Artem Pyankov <artpyankov@gmail.com>
2016-12-11 18:46:27 +0500

    Encrypt user password

--- web/models/user.ex
+++ web/models/user.ex
@@ -15,4 +15,18 @@ defmodule Rumbl.User do
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
   end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_password_hash
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
